d662275fdc1eb4488d4593dca632921593352636
Anh.Tuan <nttanh6299@gmail.com>
2024-11-02 23:59:19 +0700

    Update code

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,28 +1,12 @@
-'use client';
-
-import { useLayoutEffect } from 'react';
 import { Inspect } from '@/modules/Inspect';
-import { usePaintStore } from '@/store/paintStore';
-import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 function App() {
-  const searchParams = useSearchParams();
-  const { getCategories, getPaint } = usePaintStore();
-
-  useLayoutEffect(() => {
-    getCategories();
-  }, []);
-
-  useLayoutEffect(() => {
-    const uuid = searchParams.get('paint');
-    if (uuid) {
-      getPaint(uuid);
-    }
-  }, []);
-
   return (
     <main className="flex-1 flex flex-col">
-      <Inspect />
+      <Suspense>
+        <Inspect />
+      </Suspense>
     </main>
   );
 }
--- src/modules/Inspect/index.jsx
+++ src/modules/Inspect/index.jsx
@@ -3,6 +3,9 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { useControlStore } from '@/store/controlStore';
+import { useLayoutEffect } from 'react';
+import { usePaintStore } from '@/store/paintStore';
+import { useSearchParams } from 'next/navigation';
 import { Scene } from './Scene';
 import { Toggle } from './UI/Toggle';
 import { Sidebar } from './UI/Sidebar';
@@ -20,6 +23,20 @@ const Controls = () => {
 };
 
 export const Inspect = () => {
+  const searchParams = useSearchParams();
+  const { getCategories, getPaint } = usePaintStore();
+
+  useLayoutEffect(() => {
+    getCategories();
+  }, []);
+
+  useLayoutEffect(() => {
+    const uuid = searchParams.get('paint');
+    if (uuid) {
+      getPaint(uuid);
+    }
+  }, []);
+
   return (
     <div className="flex-1 flex flex-row">
       <Canvas flat shadows dpr={[1, 2]} camera={{ position: [1.5, 0.3, 0], fov: 40 }}>
